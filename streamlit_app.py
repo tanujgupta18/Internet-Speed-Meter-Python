00e3b9445b4a7558b543c9fb49bd46869bb586b0
Tanuj Gupta <64638288+tanujgupta18@users.noreply.github.com>
2023-10-22 23:29:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import speedtest
-import time
 
 def speedcheck():
-    st.write("Testing your internet speed. Please wait...")
+    st.write("Testing internet speed... Please wait.")
     sp = speedtest.Speedtest()
-    download_speed = sp.download()
-    upload_speed = sp.upload()
-    
-    st.write("Download Speed: {:.2f} Mbps".format(download_speed / 10**6))
-    st.write("Upload Speed: {:.2f} Mbps".format(upload_speed / 10**6))
+    sp.get_servers()
+    download_speed = sp.download() / 1_000_000  # Convert to Mbps
+    upload_speed = sp.upload() / 1_000_000  # Convert to Mbps
+    st.write(f"Download Speed: {download_speed:.3f} Mbps")
+    st.write(f"Upload Speed: {upload_speed:.3f} Mbps")
 
 st.title("Internet Speed Test")
-
 st.markdown("### Download and Upload Speed Test")
 
 if st.button("Check Speed"):
