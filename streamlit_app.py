5aac1645f91a21e31970c75585c3fc4c746e041f
Tanuj Gupta <64638288+tanujgupta18@users.noreply.github.com>
2023-12-15 10:30:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,29 @@
 import streamlit as st
 import speedtest
 
-@st.cache(ttl=300)  #timeout for 300 seconds (5 minutes)
+@st.cache(ttl=300)  # Set a timeout for 300 seconds (5 minutes)
 def get_speed():
-    st.text("Checking speed...")
+    st.text("Checking speed...")  # Show a message outside the cached function
     sp = speedtest.Speedtest()
     sp.get_servers()
     down = str(round(sp.download() / (10**6), 3)) + " Mbps"
     up = str(round(sp.upload() / (10**6), 3)) + " Mbps"
-    st.text("Speed check complete.") 
     return down, up
 
+# Streamlit UI
 st.title("Internet Speed Test")
 
 # Download Speed Section
 st.subheader("Download Speed")
-lab_down = st.empty()
+lab_down = st.empty()  # Create a placeholder for download speed
 
 # Upload Speed Section
 st.subheader("Upload Speed")
-lab_up = st.empty()
+lab_up = st.empty()  # Create a placeholder for upload speed
 
 # Check Speed Button
 if st.button("Check Speed"):
-    down, up = get_speed()
+    down, up = get_speed()  # Call the get_speed function when the button is clicked
+    st.text("Speed check complete.")  # Show a message outside the cached function
     lab_down.text(down)
     lab_up.text(up)
